b55ba1af7d5257b17060e738ed75ef818fbbc726
Mridul Chopra <mridul.chopra@paxcel.net>
2020-02-18 17:51:07 +0530

    again 3

--- src/main/java/net/paxcel/bookshelf/dao/GetGenresDAOImpl.java
+++ src/main/java/net/paxcel/bookshelf/dao/GetGenresDAOImpl.java
@@ -1,26 +1,33 @@
-/*
- * package net.paxcel.bookshelf.dao;
- * 
- * import java.sql.ResultSet;
- * 
- * import org.apache.log4j.Logger; import
- * org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.stereotype.Repository;
- * 
- * @Repository public class GetGenresDAOImpl implements GetGenresDAO {
- * 
- * @Autowired Logger log; // getting logger
- * 
- * @Autowired QueryHelper sql; // custom sql helper
- * 
- * @Override public ResultSet getGenres() throws Exception {
- * 
- * try { String query ="Select Distinct GENRE from BOOKS ;"; sql.setQuery=
- * (Void)->{return Void;}; // setting query ResultSet result =
- * sql.getData(query); //getting result from DB
- * 
- * return result; } catch(Exception e) { log.error(this.getClass()+"-->> "+e);
- * // logging exception throw new Exception(); } }
- * 
- * }
- */
+package net.paxcel.bookshelf.dao;
+
+import java.sql.ResultSet;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GetGenresDAOImpl implements GetGenresDAO {
+
+	@Autowired Logger log;  // getting logger 
+	@Autowired QueryHelper sql; // custom sql helper
+	
+	@Override
+	public ResultSet getGenres() throws Exception {
+		
+		try
+		{
+			String query ="Select Distinct GENRE from BOOKS ;"; 
+			sql.setQuery= (Void)->{return Void;}; // setting query
+			ResultSet result = sql.getData(query);  //getting result from DB
+	
+			return result;
+		}
+		catch(Exception e)
+		{
+			log.error(this.getClass()+"-->> "+e); // logging exception
+			throw new Exception();
+		}
+	}
+
+}
